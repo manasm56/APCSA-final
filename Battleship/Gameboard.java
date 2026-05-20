3de20f79f2dcc7fa4fe722193cedfc9750543763
manasm56 <121202392+manasm56@users.noreply.github.com>
2026-05-20 03:17:38 +0000

    added some parts of gameboard class

--- Battleship/Gameboard.java
+++ Battleship/Gameboard.java
@@ -1,8 +1,25 @@
 public class Gameboard{
-    private Gameship[][] Gameboard;
+    private char[][] gBoard;
 
-    public Gameboard(){
-        
+    public Gameboard(int gSize){
+    Gameboard = new char[gSize][gSize];
+    for(int i=0; i<gBoard.length; i++){
+        for(int g=0; g<gBoard[0].length; g++){
+            gBoard[i][g]="W"
+        }
+    }    
+    public boolean placeShip(int r, int c){
+        if(r<0 || c<0 || c>=gBoard.length || r>=gBoard.length){
+            return false;
+        }
+        else if(gBoard[r][c]=='S'){
+            return false
+        }
+        else{
+            gBoard[r][c]='s';
+            return true;
+        }
+    }
     }
 
 
