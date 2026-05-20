54bc723a9afd1b111cd013a1ca16b4e6fb14a555
manasm56 <121202392+manasm56@users.noreply.github.com>
2026-05-20 20:03:21 +0000

    gameboard class

--- Battleship/Gameboard.java
+++ Battleship/Gameboard.java
@@ -23,16 +23,50 @@ public class Gameboard{
     }
     public boolean attacks(int r, int c){
         if(gBoard[r][c]=='S'){
-            gBoard='x'
+            gBoard='X'
             return true;
         }
         else if(gBoard[r][c]=='W'){
+            gBoard='O'
             return false;
         }
         else{
             return false
             System.out.println("Your guess is out of bounds, try again next turn");
         }
+    }
+    public boolean checkGuess(){
+        if(gBoard[r][c]=='X' || gBoard[r][c]=='O'){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean checkShip(){
+        for(int i=0; i<gBoard.length; i++){
+            for(int j=0; j<gBoard.length; j++){
+                if(gBoard[i][j]=='S'){
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+    public void print(boolean shipHide){
+        for(int i=0; i<gBoard.length; i++){
+            for(int j=0; j<gBoard.length; j++){
+                if(shipHide && gBoard[i][j]=='S'){
+                    System.out.print("W");
+                }
+                else{
+                    System.out.print(gBoard[i][j]+"");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
 
